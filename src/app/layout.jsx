2cf0625d101c874a5b2c3f05ee8f0abdfebc4ec7
FahadKabirKsl <mdfahadkabirsezan@gmail.com>
2024-08-20 22:40:50 +0600

    add new loader

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientWrapper from "./ClientWrapper";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ClientWrapper>{children}</ClientWrapper>
+        <Suspense fallback={<Loading />}>
+          <ClientWrapper>{children}</ClientWrapper>
+        </Suspense>
       </body>
     </html>
   );
