f9c5f8e5b37870cc65ffa597ab7f21f7f48fc2d2
FahadKabirKsl <mdfahadkabirsezan@gmail.com>
2024-09-08 14:03:45 +0600

    add product

--- src/app/become_a_distributor/page.jsx
+++ src/app/become_a_distributor/page.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-
 import DistributorFrom from "@/components/Distributor/DistributorFrom";
-import Faq from "@/components/Homepage/Faq";
-import DistributorInfo from "@/components/Distributor/DistributorInfo";
 import DistributorHero from "@/components/Distributor/DistributorHero";
 
 export default function Distributor() {
@@ -15,13 +12,6 @@ export default function Distributor() {
       <div className=" px-4 sm:px-6 md:px-8 lg:px-10 xl:px-16 mb-10">
         <DistributorFrom />
       </div>
-      {/* <div>
-        <div className="flex flex-col md:flex-row md:justify-between px-4 sm:px-6 md:px-8 lg:px-10 xl:px-16 mb-10">
-          <DistributorInfo />
-          <div className="mx-10 my-5"></div>
-          <DistributorFrom />
-        </div>
-      </div> */}
     </>
   );
 }
